1ec11e5a6452b38394be335adebd858aa20b4034
CaroMen <caroline_mendez@alumni.baylor.edu>
2021-12-02 20:59:45 -0600

    extra string methods

--- basics/basics.rb
+++ basics/basics.rb
@@ -52,11 +52,32 @@ puts "   hello   ".strip # returns "hello"
 # ------------------- THINGS TO LOOK UP --------------------
 # sub, gsub, insert, delete, prepend, gsub!
 
-# ------------------- SYMBOLS --------------------
-# strings can be changed, so when used ruby has to store it in memory even if an existing string with the same value already exists
-# symbols are only stored once in memory
-# symbols are faster than strings
-# symbols are immutable
-# symbols are typically preferred over strings as the keys in hashes
+# ----------- gsub and gsub! and sub ----------------
+=begin
+ - both gsub and gsub! are substitution methods, will find instances of the matched string and replaces it with the new argument.
+ - sub is a substitution method that will only replace the first instance of the matched string.
+ - first argument is the string to be replaced, second is the string to replace it with
+ - in simple terms, gsub will create a "temp" copy of a variable while gsub! will change the original one
+=end
+gsub_var = "Beanie"
+new_var = gsub_var.gsub("Bea", "ba") # returns "banie"
+puts "New variable, #{new_var}"
+puts "Is it the same, #{gsub_var}"
 
+# ----------- gsub and gsub! ----------------
+=begin
+  - will insert the second argument before the character at the given index
+  - will modify the original variable (?)
+=end
+puts "Ruby".insert(2, "a")
+puts "Rails".insert(5, "b")
+
+# ------------------- SYMBOLS --------------------
+=begin
+  - strings can be changed, so when used ruby has to store it in memory even if an existing string with the same value already exists
+  - symbols are only stored once in memory
+  - symbols are faster than strings
+  - symbols are immutable
+  - symbols are typically preferred over strings as the keys in hashes
+=end
 :new_symbol
