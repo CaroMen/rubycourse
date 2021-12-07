37ce30729e787118ee57cf798e21c93ec8fea68a
CaroMen <caroline_mendez@alumni.baylor.edu>
2021-12-07 07:22:56 -0600

    any?, all?, none? methods

--- basics/enumerables.rb
+++ basics/enumerables.rb
@@ -36,3 +36,41 @@ new_res = stores.reduce(Hash.new(0)) do |store_hash, store|
 end
 
 puts new_res
+
+puts " "
+puts " "
+puts " "
+
+# ----------------------------- PREDICATE ENUMERABLES ---------------------------------
+# include? method
+# can be used if we want to know whether a particular item exists in an array
+# will return true if the element passed in as an argument exists in the array
+numbers = [2, 4, 5, 6, 7, 8, 19, 23, 45, 54]
+
+# without include?
+numbers.each do |num|
+  if num == 23
+    result = true
+  end
+end
+
+# with include?
+puts numbers.include?(23)
+
+# any? method
+# returns true if any elements in the array or hash matches the condition within the block
+
+puts numbers.any? { |num| num >= 10}
+
+puts numbers.count { |num| num >= 10}
+
+# all? method
+# returns true if the condition in the block applies to all of the elements in your array
+words = ['hello', 'world', 'my', 'name', 'is', 'caro']
+
+puts words.all? { |word| word.include?("a")}
+
+# none? method
+# returns true only if the condition in the block matches none of the elements in the array
+
+puts words.none? { |word| word.include?("z")}
